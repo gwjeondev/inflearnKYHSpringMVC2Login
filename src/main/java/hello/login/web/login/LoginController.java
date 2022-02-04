c0834d68e22fd87c27201e21596cc2ce3c9c1556
won-developer <gw4609@gmail.com>
2022-02-04 20:03:52 +0900

    [로그인 처리1 - 쿠키, 세션] 로그인 처리하기 - 쿠키 사용

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -11,7 +11,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
@@ -26,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if(bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
             return "login/loginForm";
@@ -38,7 +39,26 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        //로그인 성공 처리 TODO
+        //로그인 성공 처리
+        //쿠키에 시간정를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        //쿠키만료처리 method 호출
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        //쿠키를 지우는 방법은 time을 초기화하면 된다.
+        Cookie cookie = new Cookie(cookieName, null);
+        //타임 초기화
+        cookie.setMaxAge(0);
+        //초기화된 쿠키 response에 등록
+        response.addCookie(cookie);
     }
 }
